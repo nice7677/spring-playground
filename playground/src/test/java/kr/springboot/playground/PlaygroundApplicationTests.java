c361ae631e6b98277a9e7fc204b57f892fed9285
진우 <nice7677@naver.com>
2023-12-28 22:30:26 +0900

    init

---
+++ playground/src/test/java/kr/springboot/playground/PlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.springboot.playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

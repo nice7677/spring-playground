941093d2a8b17dd98adbff4f1e7eb7deecab03e6
진우 <nice7677@naver.com>
2023-12-29 17:28:37 +0900

    retrytemplate

--- playground/src/main/java/kr/springboot/playground/retry/HttpRequestService.java
+++ playground/src/main/java/kr/springboot/playground/retry/HttpRequestService.java
@@ -2,6 +2,7 @@ package kr.springboot.playground.retry;
 
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Retryable;
+import org.springframework.retry.support.RetryTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientRequestException;
@@ -10,6 +11,7 @@ import org.springframework.web.reactive.function.client.WebClientRequestExceptio
 public class HttpRequestService {
 
     private int count = 0;
+    private int count2 = 0;
 
     @Retryable(retryFor = Exception.class, backoff = @Backoff(delay = 5000), maxAttempts = 3)
     public String request() {
@@ -35,6 +37,41 @@ public class HttpRequestService {
         String response = webClient.get().retrieve().bodyToMono(String.class).block();
 
         return response;
+    }
+
+    public String requestTemplate() {
+        RetryTemplate template = RetryTemplate.builder()
+                .maxAttempts(3)
+                .fixedBackoff(1000)
+                .retryOn(RuntimeException.class)
+                .build();
+
+        return template.execute(ctx -> {
+
+            if (count2 == 2) {
+
+                WebClient webClient = WebClient.builder()
+                        .baseUrl("https://httpbin.org/get")
+                        .build();
+                String response = webClient.get()
+                        .retrieve()
+                        .bodyToMono(String.class).doOnError(throwable -> System.out.println(throwable.getMessage())).block();
+
+                return response;
+
+            }
+
+            count2++;
+
+            WebClient webClient = WebClient.builder()
+                    .baseUrl("https://httpbin.org/get3")
+                    .build();
+            String response = webClient.get().retrieve().bodyToMono(String.class).block();
+
+            return response;
+
+        });
+
     }
 
 }
--- playground/src/test/java/kr/springboot/playground/retry/HttpRequestServiceTest.java
+++ playground/src/test/java/kr/springboot/playground/retry/HttpRequestServiceTest.java
@@ -1,10 +1,16 @@
 package kr.springboot.playground.retry;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.InstanceOfAssertFactories.MAP;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -15,11 +21,33 @@ class HttpRequestServiceTest {
     @Autowired
     HttpRequestService httpRequestService;
 
+    private ObjectMapper objectMapper = new ObjectMapper();
+
     @Test
     void request() {
         String response = httpRequestService.request();
         System.out.println(response);
-        assertThat(response).isNotNull();
+        Map<String, String> responseBody = null;
+        try {
+            responseBody = objectMapper.readValue(response, Map.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        assertThat(responseBody).isNotEmpty();
+        assertThat(responseBody.get("url")).isEqualTo("https://httpbin.org/get");
+    }
+
+    @Test
+    void templateRequest() {
+        String response = httpRequestService.requestTemplate();
+        Map<String, String> responseBody = null;
+        try {
+            responseBody = objectMapper.readValue(response, Map.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        assertThat(responseBody).isNotEmpty();
+        assertThat(responseBody.get("url")).isEqualTo("https://httpbin.org/get");
     }
 
 }
